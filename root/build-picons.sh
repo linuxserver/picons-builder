78d484d3e066f0c5414ff572097cd291c588e41b
sparklyballs <sparklyballs@gmail.com>
2016-11-13 15:16:08 +0000

    add bzip2 package back, copy only hardlink to output with fixed name

--- root/build-picons.sh
+++ root/build-picons.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # clear out any potential stale folders in /tmp
 rm -rf /tmp/*
 
@@ -10,5 +11,8 @@ git clone https://github.com/picons/picons-source.git /usr/src/picons-source
 cp /backgrounds.conf /usr/src/picons-source/build-input/
 
 # build picons
-cd /usr/src/picons-source
+cd /usr/src/picons-source || exit
 /bin/bash 2-build-picons.sh snp-full
+
+# copy picons archive to /picons folder
+cp -f /usr/src/picons-source/build-output/binaries-snp-full/*hardlink*.tar.bz2 /picons/picons.tar.bz2
