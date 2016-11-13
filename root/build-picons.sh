ddb43f849c7420bbab131f52d0e7fc8f51f60157
tobbenb <torbjornbrekke@gmail.com>
2016-11-13 13:44:04 +0100

    Added backgrounds.conf

--- root/build-picons.sh
+++ root/build-picons.sh
@@ -6,6 +6,9 @@ rm -rf /tmp/*
 # clone picon repository
 git clone https://github.com/picons/picons-source.git /usr/src/picons-source
 
+# copy backgrounds.conf to build-input
+cp /backgrounds.conf /usr/src/picons-source/build-input/
+
 # build picons
 cd /usr/src/picons-source
 /bin/bash 2-build-picons.sh snp-full
