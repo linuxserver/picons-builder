e4c374591442548f3736d1a919b085957b41f964
sparklyballs <sparklyballs@gmail.com>
2016-11-13 16:07:17 +0000

    unpack and repack picons tar, copy just tar to output

--- root/build-picons.sh
+++ root/build-picons.sh
@@ -14,5 +14,10 @@ cp /backgrounds.conf /usr/src/picons-source/build-input/
 cd /usr/src/picons-source || exit
 /bin/bash 2-build-picons.sh snp-full
 
-# copy picons archive to /picons folder
-cp -f /usr/src/picons-source/build-output/binaries-snp-full/*hardlink*.tar.bz2 /picons/picons.tar.bz2
+# unpack picons to temp folder
+tar xf \
+	/usr/src/picons-source/build-output/binaries-snp-full/*hardlink*.tar.bz2 -C \
+	/usr/src/temp-output --strip-components=1
+
+# repack picons in path-free tar in picons folder
+tar -cjf /picons/picons.tar.bz2 -C /usr/src/temp-output .
